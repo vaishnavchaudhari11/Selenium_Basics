7e04069886f4caf401d1b58071725771e5ca1190
Vaishnav Chaudhari <vaishnavchaudhari11@gmail.com>
2026-06-18 22:22:13 +0530

    Initial commit

---
+++ src/main/java/demo/basics.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class basics {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+	}
+
+}
